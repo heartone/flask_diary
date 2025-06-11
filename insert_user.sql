3170f5e71cb8cbfae66d665b2bfa24bd8d9dbb3d
原宏之 <heartone@mba.local>
2025-06-11 21:25:31 +0900

    passwordとnameのカラムの位置を間違えていたので修正

--- insert_user.sql
+++ insert_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO users(email, password, name) VALUES('yamada@example.com', '山田太郎', 'himitu');
-INSERT INTO users(email, password, name) VALUES('suzuki@example.com', '鈴木花子', 'himitu');
+INSERT INTO users(email, password, name) VALUES('yamada@example.com', 'himitu', '山田太郎');
+INSERT INTO users(email, password, name) VALUES('suzuki@example.com', 'himitu', '鈴木花子');
