0dc0663a30e91033350c64bd118ab2734b0d0767
原宏之 <heartone@mba.local>
2025-06-12 05:11:37 +0900

    nullable間違ってた。

--- lib/models/diary.py
+++ lib/models/diary.py
@@ -4,9 +4,9 @@ class Diary(db.Model):
     __tablename__ = 'diaries'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
-    title = db.Column(db.String(50), nullable=True)
-    content = db.Column(db.Text, nullable=True)
-    created_at = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
     user = db.relationship('User', uselist=False)
