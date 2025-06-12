57508b2d931eb20b386d307cfa73bbe840826951
原宏之 <heartone@mba.local>
2025-06-12 12:35:29 +0900

    diariesテーブルにis_publicカラムを追加

--- lib/models/diary.py
+++ lib/models/diary.py
@@ -7,6 +7,7 @@ class Diary(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     title = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    is_public = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
     user = db.relationship('User', uselist=False)
